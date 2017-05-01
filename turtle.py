265298345909d19b9b50b7daea59b215c871b3ca
201710907 <a981024@naver.com>
2017-05-01 16:41:13 +0900

    Add files via upload

---
+++ turtle.py
@@ -0,0 +1,13 @@
+import turtle
+
+wn = turtle.Screen()
+t1 = turtle.Turtle()
+
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
